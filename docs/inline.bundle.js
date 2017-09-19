6a7e0e6b6c0ef0a3b922cab730eebd59f8d028f8
slangberg <samuel.langberg@gmail.com>
2017-09-19 15:11:01 -0400

    t

--- docs/inline.bundle.js
+++ docs/inline.bundle.js
@@ -141,7 +141,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://slangberg.github.io/ng-simple-grid/";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
